be58d2942be353c685e15676119196df6df788b8
Foster Stule <camodude5@yahoo.com>
2019-05-02 14:55:11 -0400

    added a contact me component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule
