5ef1a5c105ff6c2aaf2296121d7d938ae6d4efbf
Foster Stule <camodude5@yahoo.com>
2019-06-01 14:19:05 -0400

    added a skills list to present on the first page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+
+  skills: string[] = [
+    'C#',
+    'Sql',
+    'EntityFramework',
+    'Angular',
+    'Javascript',
+    'Typescript',
+    'CSS',
+    'HTML',
+    'MVC',
+    'Python',
+    'Communication Skills',
+    'Scrum Certified',
+    'Easilly Adaptable',
+  ];
 
   constructor() { }
 
